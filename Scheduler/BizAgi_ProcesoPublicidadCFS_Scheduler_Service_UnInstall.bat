af59b3939228e4c3d5c2268f329831e7afa4b702
unknown <gnsosam@utn.edu.ec>
2024-10-29 13:04:51 -0500

    first commit

---
+++ Scheduler/BizAgi_ProcesoPublicidadCFS_Scheduler_Service_UnInstall.bat
@@ -0,0 +1,3 @@
+"C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /u /name=BizAgiProcesoPublicidadCFSSchedulerService BizAgi.Scheduler.Services.exe
+sc delete BizAgiProcesoPublicidadCFSSchedulerService
+rem pause
